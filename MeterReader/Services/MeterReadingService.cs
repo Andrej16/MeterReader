a08edddc78b46d6871d009726177e788ef1fa7b9
Andrii Shylin <31798010+Andrej16@users.noreply.github.com>
2025-03-28 16:31:40 +0200

    Add project files.

---
+++ MeterReader/Services/MeterReadingService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using MeterReader.gRPC;
+using static MeterReader.gRPC.MeterReaderService;
+
+namespace MeterReader.Services;
+
+public class MeterReadingService : MeterReaderServiceBase
+{
+    public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+    {
+        if (request.Successful == ReadingStatus.Success)
+        {
+            return new StatusMessage()
+            {
+                Notes = "Successfully added to the database.",
+                Status = ReadingStatus.Success
+            };
+        }
+
+        return new StatusMessage()
+        {
+            Notes = "Failed to store readings in Database",
+            Status = ReadingStatus.Success
+        };
+    }
+}
