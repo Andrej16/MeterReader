f79020addcf8a89e6160aba8bba6b80b884d1ca1
Andrii Shylin <31798010+Andrej16@users.noreply.github.com>
2025-04-02 18:11:50 +0300

    Added into Worker implementation of calling gRPC service

--- MeterReader/Services/MeterReadingService.cs
+++ MeterReader/Services/MeterReadingService.cs
@@ -6,16 +6,27 @@ namespace MeterReader.Services;
 
 public class MeterReadingService : MeterReaderServiceBase
 {
+    private readonly ILogger<MeterReadingService> _logger;
+
+    public MeterReadingService(ILogger<MeterReadingService> logger)
+    {
+        _logger = logger;
+    }
+
     public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
     {
         if (request.Successful == ReadingStatus.Success)
         {
+            _logger.LogInformation("Successfully saved.");
+            
             return new StatusMessage()
             {
                 Notes = "Successfully added to the database.",
                 Status = ReadingStatus.Success
             };
         }
+
+        _logger.LogError("Failed to save.");
 
         return new StatusMessage()
         {
--- WorkerService1/Worker.cs
+++ WorkerService1/Worker.cs
@@ -1,17 +1,23 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using MeterReader.gRPC;
+using static MeterReader.gRPC.MeterReaderService;
+
 namespace WorkerService1;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
-    private readonly ReadingGenerator _readingGenerator;
     private readonly int _customerId;
+    private readonly string _serviceUrl;
 
-    public Worker(ILogger<Worker> logger, ReadingGenerator readingGenerator,
+    public Worker(
+        ILogger<Worker> logger, 
         IConfiguration configuration)
     {
         _logger = logger;
-        _readingGenerator = readingGenerator;
         _customerId = configuration.GetValue<int>("CustomerId");
+        _serviceUrl = configuration["ServiceUrl"] ?? throw new ArgumentNullException();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -19,6 +25,32 @@ public class Worker : BackgroundService
         while (!stoppingToken.IsCancellationRequested)
         {
             // Call the gRPC service
+            var channel = GrpcChannel.ForAddress(_serviceUrl);
+            var client = new MeterReaderServiceClient(channel);
+
+            var packet = new ReadingPacket() { Successful = ReadingStatus.Success };
+
+            for (var i = 0; i < 5; i++)
+            {
+                var reading = new ReadingMessage()
+                {
+                    CustomerId = _customerId,
+                    ReadingTime = Timestamp.FromDateTime(DateTime.UtcNow),
+                    ReadingValue = new Random().Next(1000)
+                };
+
+                packet.Readings.Add(reading);
+            }
+
+            var statusMessage = client.AddReading(packet);
+            if (statusMessage.Status == ReadingStatus.Success)
+            {
+                _logger.LogInformation("Successfully called gRPC.");
+            }
+            else
+            {
+                _logger.LogError("Failed to call gRPC.");
+            }
 
             await Task.Delay(1000, stoppingToken);
         }
